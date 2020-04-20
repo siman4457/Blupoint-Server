0ac5609d4c5b87de063591b332fb826a5b8544c3
Siman <siman4457@gmail.com>
2020-04-20 00:57:55 -0500

    styled config page

--- client/src/components/config/RemoveIDCard.js
+++ client/src/components/config/RemoveIDCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
 import axios from 'axios'
 
 export default class AddSensor extends Component {
@@ -36,49 +35,36 @@ export default class AddSensor extends Component {
 
     };
 
+    handleCancel = () => {
+        this.setState({
+            idCardID: ''
+        })
+    }
+
     render() {
         return (
-            <div className="modal-container">
-                <div>
-                    <div className="modal-background"></div>
-                    <div className="modal-card">
-                        <header className="modal-card-head">
-                            <p className="modal-card-title">Remove an ID Card</p>
-                            <Link to={'/config'}
-                                className="delete"
-                                aria-label="close"
-                            ></Link>
-                        </header>
-                        <section className="modal-card-body">
-                            {/* <h1 className="title">Add a new sensor</h1> */}
-                            <div className="field">
-                                <p className="control has-icons-left has-icons-right">
-                                    <input
-                                        name="idCardID" className="input" type="text" placeholder="Enter ID Card Number"
-                                        value={this.state.idCardID}
-                                        onChange={this.handleChange}
-                                    />
-                                    <span className="icon is-small is-left">
-                                        <i className="fas fa-envelope"></i>
-                                    </span>
-                                    <span className="icon is-small is-right">
-                                        <i className="fas fa-check" />
-                                    </span>
-                                </p>
-                            </div>
-
-                        </section>
-                        <footer className="modal-card-foot">
-                            <button className="button is-danger" onClick={this.handleSubmit}>
-                                Remove
-                            </button>
-                            <Link className="button" to={"/config"}>
-                                Cancel
-                            </Link>
-                        </footer>
+            <div style={{ padding: '20px' }}>
+                <section >
+                    <h1 className="title">Remove an ID card</h1>
+                    <div className="field">
+                        <input
+                            name="idCardID" className="input" type="text" placeholder="Enter ID Card Number"
+                            value={this.state.idCardID}
+                            onChange={this.handleChange}
+                        />
                     </div>
-                </div>
-            </div >
+                </section>
+                <br />
+                <footer>
+                    <button className="button is-danger" onClick={this.handleSubmit}>
+                        Save
+                </button>
+               &emsp;
+                <button className="button" onClick={this.handleCancel}>
+                        Cancel
+                </button>
+                </footer>
+            </div>
         );
     }
 }
